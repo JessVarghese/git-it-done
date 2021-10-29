cf6a9f3153dc53a55bc1c197669c354bc58c2ef3
Jess Varghese <jessvarghese@gmail.com>
2021-10-28 20:08:13 -0400

    add error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,22 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+      }
+   
+  };
+
+
 let getRepoIssues = function(repo) {
 console.log(repo);
 
@@ -18,7 +34,7 @@ let apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
             });
         }
         else {
-            alert('There was a problem with your request!');
+            document.location.replace("./index.html");
         }
     });
 
@@ -82,4 +98,4 @@ var displayWarning = function(repo) {
 };
 
 
-getRepoIssues("facebook/react");
+getRepoName();
